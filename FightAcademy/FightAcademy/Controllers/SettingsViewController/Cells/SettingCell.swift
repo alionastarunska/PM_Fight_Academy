31c09e753da16c5bfed05e4a70175ae6b1fa2b44
Sebastian <rather.not.say.my.mail@gmail.com>
2021-03-26 21:05:50 +0200

    Settings screen added

---
+++ FightAcademy/FightAcademy/Controllers/SettingsViewController/Cells/SettingCell.swift
@@ -0,0 +1,12 @@
+//
+//  SettingCell.swift
+//  FightAcademy
+//
+//  Created by Artem Myshkin on 3/26/21.
+//
+
+import UIKit
+
+protocol SettingCell: ConfigurableTableViewCell, ReusableTableViewCell where Item == SettingModel {
+
+}
